415534e241e8fc87d6d89fdbfce0c93d25acddf7
atomfrede <atomfrede@gmail.com>
2017-05-19 20:07:28 +0200

    use jhipster cli and align with zsh plugin

--- conf.d/jhipster.aliases.fish
+++ conf.d/jhipster.aliases.fish
@@ -1,28 +1,30 @@
-alias jhyarn='yo jhipster --yarn'
-alias jhskip='yo jhipster --skip-install'
-alias jhinstall='npm install; and bower install; and gulp install'
-alias jhupgrade='yo jhipster:upgrade'
-alias jhf='yo jhipster --force'
-alias jhfe='yo jhipster --force --with-entities'
+alias jh='jhipster'
+alias jhyarn='jhipster --yarn'
+alias jhnpm='jhipster --npm'
+alias jhskip='jhipster --skip-install --skip-checks'
+alias jhf='jhipster --force'
+alias jhfe='jhipster --force --with-entities'
 
-alias jhjdl='yo jhipster:import-jdl'
-alias jhe='yo jhipster:entity'
-alias jhs='yo jhipster:service'
-alias jhlang='yo jhipster:languages'
-alias jhinfo='yo jhipster:info'
-alias jhcompose='yo jhipster:docker-compose'
-alias jhrancher='yo jhipster:rancher-compose'
-alias jhcicd='yo jhipster:ci-cd'
+alias jhupgrade='jhipster upgrade'
+alias jhjdl='jhipster import-jdl'
+alias jhe='jhipster entity'
+alias jhs='jhipster service'
+alias jhlang='jhipster languages'
+alias jhinfo='jhipster info'
+alias jhcompose='jhipster docker-compose'
+alias jhrancher='jhipster rancher-compose'
+alias jhcicd='jhipster ci-cd'
 
-alias jhcf='yo jhipster:cloudfoundry'
-alias jhheroku='yo jhipster:heroku'
-alias jhkubernetes='yo jhipster:kubernetes'
-alias jhaws='yo jhipster:aws'
+alias jhcf='jhipster cloudfoundry'
+alias jhheroku='jhipster heroku'
+alias jhkubernetes='jhipster kubernetes'
+alias jhaws='jhipster aws'
+alias jhopenshift='jhipster openshift'
 
 function jhclean
     if test -e mvnw
         ./mvnw clean
-    else
+    else test -e gradlew
         ./gradlew clean --no-daemon
     end
 end
@@ -30,7 +32,7 @@ end
 function jhrun
     if test -e mvnw
         ./mvnw spring-boot:run
-    else
+    else test -e gradlew
         ./gradlew bootRun --no-daemon
     end
 end
@@ -38,7 +40,7 @@ end
 function jhpack
     if test -e mvnw
         ./mvnw -Pprod package
-    else
+    else if test -e gradlew
         ./gradlew -Pprod bootRepackage --no-daemon
     end
 end
@@ -46,7 +48,7 @@ end
 function jhdock
     if test -e mvnw
         ./mvnw -Pprod package docker:build
-    else
+    else if test -e gradlew
         ./gradlew -Pprod bootRepackage buildDocker --no-daemon
     end
 end
@@ -54,32 +56,43 @@ end
 function jhgatling
     if test -e mvnw
         ./mvnw gatling:execute
-    else
+    else if test -e gradlew
         ./gradlew gatlingRun --no-daemon
     end
 end
 
+alias jhappup='docker-compose -f src/main/docker/app.yml up -d'
+alias jhappdown='docker-compose -f src/main/docker/app.yml down'
+alias jhappstop='docker-compose -f src/main/docker/app.yml stop'
+alias jhapplogs='docker-compose -f src/main/docker/app.yml logs --follow'
 alias jhmysqlup='docker-compose -f src/main/docker/mysql.yml up -d'
 alias jhmysqldown='docker-compose -f src/main/docker/mysql.yml down'
 alias jhmysqlstop='docker-compose -f src/main/docker/mysql.yml stop'
+alias jhmysqllogs='docker-compose -f src/main/docker/mysql.yml logs --follow'
 alias jhmariaup='docker-compose -f src/main/docker/mariadb.yml up -d'
 alias jhmariadown='docker-compose -f src/main/docker/mariadb.yml down'
 alias jhmariastop='docker-compose -f src/main/docker/mariadb.yml stop'
+alias jhmarialogs='docker-compose -f src/main/docker/mariadb.yml logs --follow'
 alias jhpostgresqlup='docker-compose -f src/main/docker/postgresql.yml up -d'
 alias jhpostgresqldown='docker-compose -f src/main/docker/postgresql.yml down'
 alias jhpostgresqlstop='docker-compose -f src/main/docker/postgresql.yml stop'
+alias jhpostgresqllogs='docker-compose -f src/main/docker/postgresql.yml logs --follow'
 alias jhmongoup='docker-compose -f src/main/docker/mongodb.yml up -d'
 alias jhmongodown='docker-compose -f src/main/docker/mongodb.yml down'
 alias jhmongostop='docker-compose -f src/main/docker/mongodb.yml stop'
+alias jhmongologs='docker-compose -f src/main/docker/mongodb.yml logs --follow'
 alias jhcassandraup='docker-compose -f src/main/docker/cassandra.yml up -d'
 alias jhcassandradown='docker-compose -f src/main/docker/cassandra.yml down'
 alias jhcassandrastop='docker-compose -f src/main/docker/cassandra.yml stop'
+alias jhcassandralogs='docker-compose -f src/main/docker/cassandra.yml logs --follow'
 alias jhesup='docker-compose -f src/main/docker/elasticsearch.yml up -d'
 alias jhesdown='docker-compose -f src/main/docker/elasticsearch.yml down'
 alias jhesstop='docker-compose -f src/main/docker/elasticsearch.yml stop'
+alias jheslogs='docker-compose -f src/main/docker/elasticsearch.yml logs --follow'
 alias jhregistryup='docker-compose -f src/main/docker/jhipster-registry.yml up -d'
 alias jhregistrydown='docker-compose -f src/main/docker/jhipster-registry.yml down'
 alias jhregistrystop='docker-compose -f src/main/docker/jhipster-registry.yml stop'
+alias jhregistrylogs='docker-compose -f src/main/docker/jhipster-registry.yml logs --follow'
 alias jhkafkaup='docker-compose -f src/main/docker/kafka.yml up -d'
 alias jhkafkadown='docker-compose -f src/main/docker/kafka.yml down'
 alias jhkafkastop='docker-compose -f src/main/docker/kafka.yml stop'
